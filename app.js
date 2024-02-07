1e472f685cbbf95b739dd718627f39d7678bab5f
Suman-sadhu-khan <su2222man@gmail.com>
2024-02-07 17:58:41 +0530

    home page updated

--- app.js
+++ app.js
@@ -28,6 +28,7 @@ app.use(methodOverride("_method"));
 app.engine("ejs",ejsMate);
 app.use(express.static(path.join(__dirname,"/public")));
 
+// const MONGODB_URL="mongodb://127.0.0.1:27017/wanderlust";
 const dbUrl=process.env.ATLASDB_URL;
 main().then(()=>{
     console.log("connected to db");
@@ -64,9 +65,9 @@ const sessionOption={
     },
 };
 
-// app.get("/",(req,res)=>{
-//     res.send("hi i am root");
-// });
+app.get("/",(req,res)=>{
+    res.render("listings/home.ejs");
+});
 
 
 
